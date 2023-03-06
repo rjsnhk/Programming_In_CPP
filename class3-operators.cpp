3179da178e54460bcd2fc5153c6d5f8823c88e0b
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-07 00:44:54 +0530

    Add files via upload

---
+++ class3-operators.cpp
@@ -0,0 +1,45 @@
+// comments -- // , /*   */
+#include <iostream> //#include header file ko kro
+using namespace std;
+int main(){
+    cout<<"hello"<< endl <<"hii"; //next line == endl
+
+    int a=4,c;
+    a=5;
+    c=89;
+    cout<< a <<c;
+
+
+
+    //datatype==primary,derived,user defined
+    //primary = int,char,float,bool,double float,void,wide character
+    //int=4byte
+    
+    //float=4byte = 7decimalplace
+    //double float=8byte = 15decimalplace
+    //bool=1byte
+    //void= nothing=0byte
+    //char=1 ='r'
+    //wide char=2
+    //string name="rajesh"
+
+
+    //derived=function,array,pointer,reference
+
+//user defined==class,structer,union,enum,typedef
+
+//1 byte=8bits
+//4 bytes= 2^(8*4)
+
+
+
+//input from
+int x;
+cin>>x;
+cout<< "hii"<<x;
+
+int y,z;
+cin>>y>>z;
+cout<< "hii"<<y<<"world"<<z;
+    return 0;
+}
