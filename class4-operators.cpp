eecbd699832494173a21cc45ebbe809e2bb6d039
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-08 23:57:24 +0530

    Add files via upload

---
+++ class4-operators.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //arith matic=+,-,*,/,%
+    int a=10,b=2;
+    // cout<<a+b<<endl;
+    // cout<<a-b<<endl;
+    // cout<<a*b<<endl;
+    // cout<<a/b<<endl;
+    // cout<<a%b<<endl;
+
+    //relational - ==,!,!=,<,>,<=,>=
+    // cout<< (a==b) << !a << (a!=b); //001
+    
+    //logical= && , || ,!
+
+    //assignment--> =, += ,-=,*=,/=,%= --->a=a+1
+    // 1,2,4,8,16,32,64,128
+    //bitwise= <<(ls),>>(rs),~,|,&,^
+    // <<=a*2^b
+    //>>=a/2^b
+    //~a=-(a+1)
+    // cout<< a<<b << (a>>b) << ~a<<endl;
+    // cout<< (a|b) << (a&b) << (a^b);
+
+
+//misc operator== sizeof(a), cond?ex1:ex2 ,comma(,)
+
+
+
+
+//dot and arrow,casting(change float to int like (int (b)))
+
+//address = &
+
+//pointer = *a
+
+
+// int z=4;
+// float x=4;
+// char ch='A';
+// cout<<sizeof(ch)<<endl;
+// cout<<(&z)<<endl;
+
+// char name='a';
+// bool flag;
+// a==name?flag= true:flag=false;
+// cout<<flag<<endl;
+
+
+//unary== +,-,++,--,!
+
+// post == a++
+// pre == ++a
+
+cout<<4+2+"abs"; //6raj
+cout<<"pqr"+4+2; //pqr42
+
+    return 0;
+}
