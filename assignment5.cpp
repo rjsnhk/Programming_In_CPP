97020cb9728a7802250d05d16d15d4036b265b08
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-11 00:08:51 +0530

    Update assignment5.cpp

--- assignment5.cpp
+++ assignment5.cpp
@@ -100,3 +100,39 @@
 
 //     return 0;
 // }
+
+
+
+//q6
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"enter your marks"<<endl;
+    int m;
+    cin>>m;
+    if(m<=100&&m>=90){
+        cout<<"A+";
+    }
+    else if(m<90&&m>=80){
+        cout<<"A";
+    }
+    else if(m<80&&m>=70){
+        cout<<"B+";
+    }
+    else if(m<70&&m>=60){
+        cout<<"B";
+    }
+    else if(m<60&&m>=50){
+        cout<<"C";
+    }
+    else if(m<50&&m>=40){
+        cout<<"D";
+    }
+    else if(m<40&&m>=30){
+        cout<<"E";
+    }
+    else if(m<30&&m>=0){
+        cout<<"F";
+    }
+    return 0;
+}
