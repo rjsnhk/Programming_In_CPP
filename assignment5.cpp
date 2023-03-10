af8b4aab5faedf66022d127d0be1d317d562fc6d
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-10 23:58:36 +0530

    Add files via upload

---
+++ assignment5.cpp
@@ -0,0 +1,102 @@
+// q1
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int l,b;
+//     cout<<"enter length"<<endl;
+//     cin>>l;
+//     cout<<"enter breadth"<<endl;
+//     cin>>b;
+//     if(l==b){
+//         cout<<("this is square");
+//     }
+//     else{
+//         cout<<"not square";
+//     }
+//     return 0;
+// }
+
+//q2
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"enter a num"<<endl;
+//     int a;
+//     cin>>a;
+//     if(a>0){
+//         cout<<a;
+//     }
+//     else{
+//         cout<<a*(-1);
+//     }
+//     return 0;
+// }
+
+//q3
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int c,s;
+//     cout<<"enter cost"<<endl;
+//     cin>>c;
+//     cout<<"enter selling price"<<endl;
+//     cin>>s;
+//     if(s>=c){
+//         cout<<"profit is "<<s-c;
+//     }
+//     else{
+//         cout<<"loss is "<<c-s;
+//     }
+    
+    
+//     return 0;
+// }
+
+// q4
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"enter a num"<<endl;
+//     int a;
+//     cin>>a;
+//     if(a>0){
+//         cout<<"it is positive number";
+//     }
+//     else{
+//         cout<<"the num is negative so skiped";
+//     }
+//     return 0;
+// }
+
+// q5
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char op;
+//     cout<<"enter an operator"<<endl;
+//     cin >> op;
+//     float a,b;
+//     cout<<"enter 2 num"<<endl;
+//     cin>>a>>b;
+//     switch (op)
+//     {
+//     case '+':
+//         cout<<a+b;
+//         break;
+//     case '-':
+//         cout<<a-b;
+//         break;
+//     case '*':
+//         cout<<a*b;
+//         break;
+//     case '/':
+//         cout<<a/b;
+//         break;
+    
+//     default:
+//     cout<<"invalid";
+//         break;
+//     }
+
+//     return 0;
+// }
