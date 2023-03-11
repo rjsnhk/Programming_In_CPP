deea3c03ce30215bcb1b9f578331912338b3fd1c
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-12 00:00:13 +0530

    Add files via upload

---
+++ class6-loops.cpp
@@ -0,0 +1,97 @@
+//while loop
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i=1;
+//     while(i<10){
+//         cout<<"hello"<<endl;
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+// for loop
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1;i<10;i++){
+//         cout<<"world"<<endl;
+//     }
+//     return 0;
+// }
+
+
+// do while
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i=1;
+//     do{
+//         cout<<"raj"<<endl;
+//         i++;
+//     }while(i<10);
+//     return 0;
+// }
+
+// sum of n natural
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"enter the num"<<endl;
+//     int i,a,sum=0;
+//     cin>>a;
+//     for(i=1;i<=a;i++){
+//         sum=sum+i;
+//     }
+//     cout<<sum;
+//     return 0;
+// }
+
+// sum of n natural use do-while
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int sum=0,n;
+//     cout<<"enter a num"<<endl;
+//     cin>>n;
+//     do{
+//         sum=sum+n;
+//         n--;
+//     }while(0<n);
+//     cout<<sum<<endl;
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+// loop ke under jo variable hote he wo bahar ate hi khatam ho jate he
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i=0;
+//     for(int i=1;i<5;i++){ //loop ke under i declare hua he
+//         }
+//         cout<<i; //0
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i=0;
+    for(i=1;i<5;i++){
+        }
+        cout<<i; //5
+    return 0;
+}
+
+
+
+
