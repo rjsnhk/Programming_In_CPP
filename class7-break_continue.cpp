270f64382a08dfefb3760c534e706393cd2659ed
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-12 00:00:28 +0530

    Add files via upload

---
+++ class7-break_continue.cpp
@@ -0,0 +1,31 @@
+// break keyword
+// 5 ka multiple likho jab 7 se divisible hoga tab stop
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=5;
+//     while(true){
+//         if (n%7==0){
+//             break;
+//         }
+//         cout<< n << endl;
+//             n=n+5;
+//     }
+//     return 0;
+// }
+
+
+
+//continue keyword
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i;
+//     for(i=1;i<=50;i++){
+//         if(i%3==0){
+//             continue;
+//         }
+//         cout<<i<<",";  //use comma
+//     }
+//     return 0;
+// }
